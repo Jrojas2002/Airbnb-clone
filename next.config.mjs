64b3321fad5b2ea114f6da725543991c2296812d
Jayson <rojasjay24@gmail.com>
2024-07-05 14:08:22 -0500

    first

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import { hostname } from "os";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "a0.muscache.com",
+        protocol: "https",
+        port: "",
+      },
+      {
+        hostname: "isenymueetrlxuvqntxp.supabase.co",
+        protocol: "https",
+        port: "",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
